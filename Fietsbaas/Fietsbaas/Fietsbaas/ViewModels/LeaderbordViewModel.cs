9dd7421adfafb40da0ff98bb03c7209d74ac7a39
Danny Lao <1946110lao@zuyd.nl>
2022-01-23 20:36:17 +0100

    Leaderboard added + points added

---
+++ Fietsbaas/Fietsbaas/Fietsbaas/ViewModels/LeaderbordViewModel.cs
@@ -0,0 +1,31 @@
+﻿using Fietsbaas.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fietsbaas.ViewModels
+{
+    public class LeaderbordViewModel : BaseListViewModel<User>
+    {
+        protected override Task OnAddItemAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override Task OnItemSelectedAsync(User item)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override async Task OnRefreshAsync()
+        {
+            Items = new ObservableCollection<User>(await Db.Users.OrderBy(x => x.Points ).ToListAsync());
+        }
+
+        
+    }
+}
--- Fietsbaas/Fietsbaas/Fietsbaas/Views/LeaderboardPage.xaml.cs
+++ Fietsbaas/Fietsbaas/Fietsbaas/Views/LeaderboardPage.xaml.cs
@@ -3,18 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Fietsbaas.Models;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Fietsbaas.ViewModels;
 
 namespace Fietsbaas.Views
 {
     [XamlCompilation( XamlCompilationOptions.Compile )]
     public partial class LeaderboardPage : ContentPage
     {
+        private readonly LeaderbordViewModel _lb;
         public LeaderboardPage()
         {
             InitializeComponent();
+            BindingContext = _lb = new LeaderbordViewModel();
         }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            _lb.OnAppearing();
+            BindingContext = null;
+            BindingContext = _lb;
+        }
+
     }
+    
+    
 }
