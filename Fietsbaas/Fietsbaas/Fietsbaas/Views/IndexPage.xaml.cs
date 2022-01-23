0a25d0f9dde8e021a2360fdf06081f50a2f686f4
Danny Lao <1946110lao@zuyd.nl>
2022-01-23 21:39:07 +0100

    Index feed added

--- Fietsbaas/Fietsbaas/Fietsbaas/Views/IndexPage.xaml.cs
+++ Fietsbaas/Fietsbaas/Fietsbaas/Views/IndexPage.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Fietsbaas.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,9 +12,21 @@ namespace Fietsbaas.Views
     [XamlCompilation( XamlCompilationOptions.Compile )]
     public partial class IndexPage : ContentPage
     {
+      
+        private readonly IndexViewModel _ip;
+
         public IndexPage()
         {
             InitializeComponent();
+            BindingContext = _ip = new IndexViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            _ip.OnAppearing();
+            BindingContext = null;
+            BindingContext = _ip;
         }
     }
 }
