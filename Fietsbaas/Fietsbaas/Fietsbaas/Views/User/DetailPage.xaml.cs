f4b1a80cd1b872d0b3430f811c6ce3ae40fdea83
Danny Lao <1946110lao@zuyd.nl>
2022-01-25 13:06:34 +0100

    filepicker added

--- Fietsbaas/Fietsbaas/Fietsbaas/Views/User/DetailPage.xaml.cs
+++ Fietsbaas/Fietsbaas/Fietsbaas/Views/User/DetailPage.xaml.cs
@@ -4,9 +4,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Plugin.Media;
 
 namespace Fietsbaas.Views
 {
@@ -17,6 +18,29 @@ namespace Fietsbaas.Views
         {
             InitializeComponent();
             BindingContext = new ProfileViewModel();
+
+            PickPhoto.Clicked += async (sender, args) =>
+            {
+                if (!CrossMedia.Current.IsPickPhotoSupported)
+                {
+                    await DisplayAlert("Photos not Supported", "Permission not granted to photos", "OK");
+                    return;
+                }
+                var file = await Plugin.Media.CrossMedia.Current.PickPhotoAsync(new Plugin.Media.Abstractions.PickMediaOptions
+                {
+                    PhotoSize = Plugin.Media.Abstractions.PhotoSize.Medium
+                });
+
+                if (file == null)
+                    return;
+
+                image.Source = ImageSource.FromStream(() =>
+                {
+                    var stream = file.GetStream();
+                    file.Dispose();
+                    return stream;
+                });
+            };
         }
     }
 }
